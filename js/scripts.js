a0557fce692d5d619d3ee208a946412d53a1c4b7
Noah Schooler <noahschooler1@gmail.com>
2018-10-26 16:05:52 -0700

    add for loop to output whole numbers between zero and the inputted whole number

--- js/scripts.js
+++ js/scripts.js
@@ -8,30 +8,41 @@ function zero (number) {
   }
 }
 
-function divisibleByThree (number) {
-  if (number % 3 === 0) {
-    return "I'm sorry, Dave. I'm afraid I can't do that."
-  } else if (number % 3 ==! 3) {
-    searchForZero(number)
+var processNumber = function (number) {
+  if (number < 0) {
+    return "Please enter a whole number"
   }
 }
 
-function searchForZero (number) {
+var divisibleByThree = function (number) {
+  if (number % 3 === 0) {
+    return "I'm sorry, Dave. I'm afraid I can't do that."
+  } console.log(divisibleByThree)
+}
+
+var searchForZero = function (number) {
   var string = number.toString()
-  console.log(string);
   var stringIncludesZero = string.includes(0)
-  console.log(stringIncludesZero);
   if (stringIncludesZero === true) {
     return "Beep!"
   }
 }
 
-function searchForOne (number) {
+var searchForOne = function (number) {
   var string = number.toString()
-  console.log(string);
   var stringIncludesOne = string.includes(1)
-  console.log(stringIncludesOne);
   if (stringIncludesOne === true) {
     return "Boop!"
   }
+}
+
+var userInput = [3]
+
+function numberCycle (userInput) {
+  var range = [];
+  console.log(userInput)
+  for (var i = 0; i < userInput; i++) {
+    range.push(i)
+  }
+  return range;
 }
